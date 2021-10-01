53e994c93b0941536a7e6871912f0d0b0d74c475
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-10-01 10:43:43 -0400

    task 2

--- src/components/header.js
+++ src/components/header.js
@@ -37,6 +37,7 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+  document.querySelector(selector).appendChild(Header("Lambda Times", "October 1, 2021", "67 F"))
 }
 
 export { Header, headerAppender }
